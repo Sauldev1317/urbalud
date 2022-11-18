0e9ddf73ded3787b4f70aa105c5fb6134fef100d
Tito Velazquez <titoadrianvelazquez@hotmail.com>
2022-11-17 22:57:25 -0600

    Add maps and marker's

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st 
+from streamlit_folium import st_folium
 import numpy as np
 import pandas as pd
 import altair as alt
+import folium
 
 st.title('Urbalud')
 st.write("Proyecto que nos ayuda a determinar si un lugar es optimo para un nuevo centro de salud")
@@ -14,8 +16,7 @@ st.write(pd.DataFrame({
     'wifi': [1, 1, 1, 1]
 }))
 
-
-
+# barChart
 df = pd.DataFrame(
     np.random.randn(200, 3),
     columns=['a', 'b', 'c'])
@@ -24,3 +25,21 @@ c = alt.Chart(df).mark_circle().encode(
     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
 
 st.altair_chart(c, use_container_width=True)
+
+
+# Map GDL
+m = n = o = folium.Map(location=[20.6595, -103.3494], zoom_start = 12)
+folium.Marker(
+    [20.6595, -103.3494],
+    popup="Liberty Bell",
+    tooltip="Liberty Bell"
+).add_to(m)
+
+st_data_map1 = st_folium(m, width = 725, key = 1)
+st_data_map2 = st_folium(n, width = 725, key = 2)
+st_data_map3 = st_folium(o, width = 725, key = 3)
+
+""" - Mapa con filtros de año 2010/2020/2030
+- Los agebs estarán de colores diferentes según su cluster  
+- Sidebar con descripción de cada cluster (atributos ) y gráficas que indiquen estadística descriptiva sobre los datos de la misma base
+ - Al dar click a un ageb surge un popup que da los datos del ageb (cluster al que pertenece, descripción del cluster, datos del AGEB) """
